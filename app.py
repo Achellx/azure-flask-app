72423d1a2cc8f5521994afceadae0b0eda2bdb42
Alejandro Soto <64869366+Achellx@users.noreply.github.com>
2025-05-18 20:53:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 app = Flask(__name__)
 
@@ -13,19 +12,4 @@ def hello():
              Nuevo deploy"""
 
 if __name__ == "__main__":
-=======
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-
-def hello():
-    return """Hola desde Azure!
-             Alejandro Soto Aguirre,
-             Ing. Sistemas Computacionales,
-             8vo semestre,
-             21550303"""
-
-if __name__ == "__main__":
->>>>>>> 56187000028ed28eb4f63a834c5630f7c771e161
     app.run()
