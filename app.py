4467a4246a528f00d8eecb117ba8d0da295c76bf
Achellx <alex.orion03@gmail.com>
2025-05-18 20:34:09 -0600

    Nuevo deploy

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello():
+    return """Hola desde Azure!
+             Alejandro Soto Aguirre,
+             Ing. Sistemas Computacionales,
+             8vo semestre,
+             21550303,
+             Nuevo deploy"""
+
+if __name__ == "__main__":
+    app.run()
